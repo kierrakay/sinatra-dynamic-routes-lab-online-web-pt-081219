d1ee3216c70f08965aff61f9c59959c2e665604d
Kierra M <k.merriman32@hotmail.com>
2019-11-24 23:32:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,13 +11,12 @@ params[number].to_i
     "#{@number ** 2}"
 end 
 
-get '/say/:number/:phrase' do 
-  str=""
-@num = params[phrase].to_i
- @phrase = params[:phrase]
-    @num.times { str += "#{@phrase}\n" }
-    #\n means new line
-    str
+get '/say/:number/:phrase' do
+    
+    
+        @number = params[:number].to_i
+    @phrase = params[:phrase]
+    "#{@phrase}\n" * @number
 end 
 
 get '/say/:word1/:word2/:word3/:word4/:word5' do 
